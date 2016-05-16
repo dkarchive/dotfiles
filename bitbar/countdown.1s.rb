0ae24e5f169950b8c7f2352db1967d6cd1180d1d
dkhamsing <dkhamsing@cirruspath.com>
2016-05-16 14:40:46 -0700

    countdown update

--- bitbar/countdown.1s.rb
+++ bitbar/countdown.1s.rb
@@ -2,15 +2,24 @@
 
 require 'date'
 
+# User options
 DATE = 'may 20 2016 10am'
 EVENT = '家庭日'
+FORMAT_SHORT = true
+SYMBOL = '🗓'
 
 d = DateTime.parse DATE
 now = DateTime.now
 d = (d - now).to_i
 
+output = "#{d}d #{SYMBOL} #{EVENT}"
+
 o = d.to_s
-o << '🗓\n'
+o << SYMBOL
+o << '\n'
 o << EVENT.to_s
 o << ' | size=8'
-puts o
+
+puts FORMAT_SHORT ? o : output
+puts '---'
+puts output
